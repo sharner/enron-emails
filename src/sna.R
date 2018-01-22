bc3402770ee9c27c87a5f0a4d001089b8f8d043d
Soren Harner <soren.harner@mulesoft.com>
2018-01-22 06:46:18 -0800

    Added social network model

--- src/sna.R
+++ src/sna.R
@@ -4,21 +4,33 @@ library(tidyr)
 require(janeaustenr) # remove later
 source('../src/parse_input.R')
 
-text <- c("Because I could not stop for Death -",
-          "He kindly stopped for me -",
-          "The Carriage held but just Ourselves -",
-          "and Immortality")
-text_df <- data_frame(line = 1:4, text = text)
-text_df %>%
-  unnest_tokens(word, text)
 
 
-original_books <- austen_books() %>%
-  group_by(book) %>%
-  mutate(linenumber = row_number(),
-         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]",
-                                                 ignore_case = TRUE)))) %>%
-  ungroup()
+create_recipient_pairs <- function(headers) {
+  sender.pairs <- unlist(apply(headers, 1, function(r) {
+    r <- as.list(r)
+    sender <- r$From
+    recipients <- collect_recipients(r)
+    cat('.')
+    unlist(apply(recipients, 1, function(r) list(sender, r)))
+  }))
+  sender.pairs <- as.tibble(matrix(sender.pairs, ncol=2, byrow=TRUE))
+  names(sender.pairs) <- c('Sender', 'Recipient')
+  sender.pairs %>%
+    filter(Sender != Recipient) %>%
+    count(Sender, Recipient, sort=TRUE)
+}
 
-tidy_books <- original_books %>%
-  unnest_tokens(word, text)
+
+# for laying out graphs http://kateto.net/networks-r-igraph
+
+recipient_graph <- function(g) {
+  set.seed(2016)
+  a <- grid::arrow(type = "closed", length = unit(.15, "inches"))
+  ggraph(g, layout = "fr") +
+    geom_edge_link(aes(edge_alpha = n), show.legend = FALSE,
+                   arrow = a, end_cap = circle(.07, 'inches')) +
+    geom_node_point(color = "lightblue", size = 5) +
+    geom_node_text(aes(label = name), vjust = 1, hjust = 1) +
+    theme_void()
+}
--- src/topic_model.R
+++ src/topic_model.R
@@ -1,1 +1,73 @@
+library(dplyr)
+library(tidytext)
+library(janeaustenr)
+library(tidyr)
+library(ggplot2)
+library(igraph)
+
+austen_bigrams <- austen_books() %>%
+  unnest_tokens(bigram, text, token = "ngrams", n = 2)
+
+austen_bigrams %>%
+  count(bigram, sort = TRUE)
 # Create a topic model based on subject lines
+
+bigrams_separated <- austen_bigrams %>%
+  separate(bigram, c("word1", "word2"), sep = " ")
+
+bigrams_filtered <- bigrams_separated %>%
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word)
+
+bigram_counts <- bigrams_filtered %>% 
+  count(word1, word2, sort = TRUE)
+
+bigrams_united <- bigrams_filtered %>%
+  unite(bigram, word1, word2, sep = " ")
+bigrams_united
+
+austen_books() %>%
+  unnest_tokens(trigram, text, token = "ngrams", n = 3) %>%
+  separate(trigram, c("word1", "word2", "word3"), sep = " ") %>%
+  filter(!word1 %in% stop_words$word,
+         !word2 %in% stop_words$word,
+         !word3 %in% stop_words$word) %>%
+  count(word1, word2, word3, sort = TRUE)
+
+bigrams_filtered %>%
+  filter(word2 == "street") %>%
+  count(book, word1, sort = TRUE)
+
+bigram_tf_idf <- bigrams_united %>%
+  count(book, bigram) %>%
+  bind_tf_idf(bigram, book, n) %>%
+  arrange(desc(tf_idf))
+
+bigram_tf_idf
+
+AFINN <- get_sentiments("afinn")
+
+not_words <- bigrams_separated %>%
+  filter(word1 == "not") %>%
+  inner_join(AFINN, by = c(word2 = "word")) %>%
+  count(word2, score, sort = TRUE) %>%
+  ungroup()
+
+not_words
+
+not_words %>%
+  mutate(contribution = n * score) %>%
+  arrange(desc(abs(contribution))) %>%
+  head(20) %>%
+  mutate(word2 = reorder(word2, contribution)) %>%
+  ggplot(aes(word2, n * score, fill = n * score > 0)) +
+  geom_col(show.legend = FALSE) +
+  xlab("Words preceded by \"not\"") +
+  ylab("Sentiment score * number of occurrences") +
+  coord_flip()
+
+bigram_graph <- bigram_counts %>%
+  filter(n > 20) %>%
+  graph_from_data_frame()
+
+bigram_graph
